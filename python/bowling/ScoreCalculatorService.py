e92380bee6d0cc91cc5b317a756730b173cbf239
Alessandro Lorenzi <me@alorenzi.eu>
2022-05-24 19:04:13 +0200

    refactor: add support functions for cursor and score

--- python/bowling/ScoreCalculatorService.py
+++ python/bowling/ScoreCalculatorService.py
@@ -4,6 +4,7 @@ class ScoreCalculatorService:
     __SPARE_FRAME_SIZE = 2
     __STRIKE_FRAME_SIZE = 1
     __LAST_THROW_SIZE = 1
+    __MAX_PINS_PER_FRAME = 10
 
     def __init__(self, rolls: list[int]):
         self.rolls = rolls
@@ -29,43 +30,42 @@ class ScoreCalculatorService:
         else:
             self.__execute_regular_throw()
 
-        
     def __frame_is_spare(self) -> bool:
-        return (self.rolls[self.rolls_cursor] + self.rolls[self.rolls_cursor + 1]) == 10
+        return (
+            self.rolls[self.rolls_cursor] + self.rolls[self.rolls_cursor + 1]
+        ) == self.__MAX_PINS_PER_FRAME
 
     def __frame_is_strike(self) -> bool:
-        return self.rolls[self.rolls_cursor] == 10
+        return self.rolls[self.rolls_cursor] == self.__MAX_PINS_PER_FRAME
 
     def __is_last_throw(self) -> bool:
         return self.rolls_cursor == len(self.rolls) - 1
 
     def __execute_spare(self):
-        self.score += (
-            self.rolls[self.rolls_cursor]
-            + self.rolls[self.rolls_cursor + 1]
-            + self.rolls[self.rolls_cursor + 2]
-        )
-        self.__advance_rolls_curor(self.__SPARE_FRAME_SIZE)
+        self.__sum_next_throws_to_score(3)
+        self.__advance_rolls_cursor(self.__SPARE_FRAME_SIZE)
 
     def __execute_strike(self):
-        self.score += (
-            self.rolls[self.rolls_cursor]
-            + self.rolls[self.rolls_cursor + 1]
-            + self.rolls[self.rolls_cursor + 2]
-        )
+        self.__sum_next_throws_to_score(3)
         if self.rolls_cursor == len(self.rolls) - self.__LAST_STRIKE_FRAME_SIZE:
-            self.__advance_rolls_curor(self.__LAST_STRIKE_FRAME_SIZE)
+            self.__advance_rolls_cursor_to_end()
             return
-        self.__advance_rolls_curor(self.__STRIKE_FRAME_SIZE)
+        self.__advance_rolls_cursor(self.__STRIKE_FRAME_SIZE)
 
     def __execute_last_throw(self):
-        self.score += self.rolls[self.rolls_cursor]
-        self.__advance_rolls_curor(self.__LAST_THROW_SIZE)
+        self.__sum_next_throws_to_score(1)
+        self.__advance_rolls_cursor(self.__LAST_THROW_SIZE)
 
     def __execute_regular_throw(self):
-        self.score += self.rolls[self.rolls_cursor] + self.rolls[self.rolls_cursor + 1]
-        self.__advance_rolls_curor(self.__REGULAR_FRAME_SIZE)
+        self.__sum_next_throws_to_score(2)
+        self.__advance_rolls_cursor(self.__REGULAR_FRAME_SIZE)
 
-    def __advance_rolls_curor(self, spaces: int):
+    def __advance_rolls_cursor(self, spaces: int):
         self.rolls_cursor += spaces
 
+    def __advance_rolls_cursor_to_end(self):
+        self.rolls_cursor += len(self.rolls)
+
+    def __sum_next_throws_to_score(self, throws: int):
+        for t in range(0, throws):
+            self.score += self.rolls[self.rolls_cursor + t]
